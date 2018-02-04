f1c07423a1a9c8a73b42bafb154a1f039a52392f
Dhruv Sheth <vrockcm@gmail.com>
2018-02-04 00:04:03 -0500

    Major Website Update

--- assets/javascripts/content-data.js
+++ assets/javascripts/content-data.js
@@ -3,3 +3,9 @@ jQuery_1('.txt').html(function(i, html) {
 
   return '<span>' + chars.join('</span><span>') + '</span>';
 });
+
+jQuery_1('.skill').html(function(i, html) {
+  var chars = ["S","K","I","L","L","S"," & ","I","N","T","E","R","E","S","T","S"];
+
+  return '<span>' + chars.join('</span><span>') + '</span>';
+});
--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -26,18 +26,42 @@ $( window ).resize(function() {
 	setHeiHeight();
 	var windowHeight	= jQuery_1(window).height(),
 		windowWidth		= $( window ).width();
-
   	var heightH = windowHeight;
 	var widthH =  ((windowHeight*4)/3);
+
 		if(widthH<windowWidth){
 			while(widthH<windowWidth){
 				widthH=widthH+50;
 				heightH = ((widthH*3)/4);
 			}
 		}
+		var heightH1 = windowHeight;
+		var widthH1 =  ((windowHeight*16)/9);
+		if(widthH1<windowWidth){
+			while(widthH1<windowWidth){
+				widthH1=widthH+20;
+				heightH1 = ((widthH*9)/16);
+			}
+		}
 		
 	jQuery_1('header').css({
 		'background-size' :  widthH+'px '+ heightH + 'px',
+	});
+	jQuery_1('.skill-parallax').css({
+            'background-size' :  widthH1+'px '+ heightH1 + 'px',
+        });
+
+	jQuery_1('header').css({
+		'height' : windowHeight + 'px',
+		'background-size' :  widthH+'px '+ heightH + 'px',
+	});
+	heightDocument	= (windowHeight) + (jQuery_1('.content').height()) + (jQuery_1('footer').height());
+	// Defining the size of the element to animate
+	jQuery_1('#scroll-animate, #scroll-animate-main').css({
+		'height' :  heightDocument + 'px'
+	});
+		jQuery_1('.wrapper-parallax').css({
+		'margin-top' : windowHeight + 'px'
 	});
 });
 
@@ -45,16 +69,8 @@ $( window ).resize(function() {
 // Start Action to load the whole page
 jQuery_1(window).load(function(){
 	var windowHeight	= jQuery_1(window).height(),
-		windowWidth		= $( window ).width();
-		footerHeight	= jQuery_1('footer').height(),
-		heightDocument	= (windowHeight) + (jQuery_1('.content').height()) + (jQuery_1('footer').height());
-
-
-	// Defining the size of the element to animate
-	jQuery_1('#scroll-animate, #scroll-animate-main').css({
-		'height' :  heightDocument + 'px'
-	});
-
+		windowWidth		= $( window ).width(),
+		footerHeight	= jQuery_1('footer').height();
 	// Defining the size of the header and content elements
 		var heightH = windowHeight;
 		var widthH =  ((windowHeight*4)/3);
@@ -64,7 +80,14 @@ jQuery_1(window).load(function(){
 				heightH = ((widthH*3)/4);
 			}
 		}
-		
+		var heightH1 = windowHeight;
+		var widthH1 =  ((windowHeight*16)/9);
+		if(widthH1<windowWidth){
+			while(widthH1<windowWidth){
+				widthH1=widthH+20;
+				heightH1 = ((widthH*9)/16);
+			}
+		}		
 	jQuery_1('header').css({
 		'height' : windowHeight + 'px',
 		'background-size' :  widthH+'px '+ heightH + 'px',
@@ -72,6 +95,11 @@ jQuery_1(window).load(function(){
 
 	jQuery_1('.wrapper-parallax').css({
 		'margin-top' : windowHeight + 'px'
+	});
+	var heightDocument	= (windowHeight) + (jQuery_1('.content').height()) + (jQuery_1('footer').height());
+	// Defining the size of the element to animate
+	jQuery_1('#scroll-animate, #scroll-animate-main').css({
+		'height' :  (heightDocument-20) + 'px'
 	});
 
 	// Getting function scrollFooter() to verify the position of the scroll and hide the footer
@@ -86,10 +114,14 @@ jQuery_1(window).load(function(){
 			'top' : '-' + scroll + 'px'
 		});
 		jQuery_1('footer').css({
-			'background-position-y' : 100 - (scroll * 600 / heightDocument) + 'px'
+			'background-position-y' : 100 - (scroll * 300 / heightDocument) + 'px'
 		});//
 		jQuery_1('header').css({
             'background-position' : 'center ' + (- scroll * 300 / heightDocument) + 'px'  
+        });
+        jQuery_1('.skill-parallax').css({
+            'background-position' : 'center ' + ((- scroll * 500) / (heightDocument)) + 'px',
+            'background-size' :  widthH1+'px '+ heightH1 + 'px',
         });
 		// On scrolling using function scrollFooter() to verify the position of the scroll and hide the footer
 		scrollFooter(scroll, footerHeight);
@@ -104,3 +136,39 @@ $(window).scroll(function(){
 $(window).scroll(function(){
     $(".name-ani").css("opacity", 1 - $(window).scrollTop() / 550);
   });
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// Navigation Scroll - ljepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
